598d486e06c8968ea9fc6e7b18bf6447b6ad357d
tientran <tientran.home@gmail.com>
2020-09-16 19:57:28 +0700

    first push project

---
+++ instagram/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by tientran on 9/16/20.
+//  Copyright © 2020 tientran. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ instagram/Controllers/Other/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  instagram
+//
+//  Created by tientran on 9/16/20.
+//  Copyright © 2020 tientran. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
